d380c6c9bd953e7b0280b2b411ede843570d0422
yanggak12 <yanggak12@naver.com>
2023-03-21 01:43:00 +0900

    feat : add layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { TopNav } from './components/Layouts';
 import { Products } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
+      <TopNav />
       <Routes>
         <Route path="/products" Component={Products} />
       </Routes>
---
+++ client/src/components/Layouts/TopNav.tsx
@@ -0,0 +1,20 @@
+import CartIcon from '../Icons/CartIcon';
+
+const TopNav = () => {
+  return (
+    <nav className="nav">
+      <div className="nav-content">
+        <div className="nav-title">
+          <CartIcon fill="#fff" size="lg" />
+          NEXTSTEP
+        </div>
+        <div className="nav-buttons">
+          <button className="nav-button">장바구니</button>
+          <button className="nav-button">주문목록</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default TopNav;
