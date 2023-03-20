1182f6fa67a0816c28b5aced65d07ded56329e86
yanggak12 <yanggak12@naver.com>
2023-03-21 02:09:06 +0900

    feat : product item

---
+++ client/src/components/Products/ProductItem.tsx
@@ -0,0 +1,28 @@
+import CartIcon from '../Icons/CartIcon';
+
+interface ProductItemProps {
+  name: string;
+  price: string;
+  imgSrc: string;
+}
+
+const ProductItem = ({ name, price, imgSrc }: ProductItemProps) => {
+  return (
+    <div className="product-info">
+      <img
+        src={imgSrc}
+        alt=""
+        style={{ background: '#c7c7c7', width: '282px', height: '282px' }}
+      />
+      <div className="product-info-bottom">
+        <div>
+          <div className="product-info__name">{name}</div>
+          <div className="product-info__price">{price} 원</div>
+        </div>
+        <CartIcon />
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -1,5 +1,17 @@
+import { ProductItem } from 'src/components/Products';
+
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div className="product-container">
+      <ProductItem name="보틀" price="43,000" imgSrc="" />
+      <ProductItem name="보틀" price="43,000" imgSrc="" />
+      <ProductItem name="보틀" price="43,000" imgSrc="" />
+      <ProductItem name="보틀" price="43,000" imgSrc="" />
+      <ProductItem name="보틀" price="43,000" imgSrc="" />
+      <ProductItem name="보틀" price="43,000" imgSrc="" />
+      <ProductItem name="보틀" price="43,000" imgSrc="" />
+    </div>
+  );
 };
 
 export default Products;
