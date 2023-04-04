900bc7e57fdd6d0cae8670153d93f48c2c11cf70
Yoonsang Yang <yanggak12@naver.com>
2023-04-04 20:26:18 +0900

    feat : add storybook

---
+++ packages/client/src/components/Layouts/TopNav/TopNav.stories.tsx
@@ -0,0 +1,11 @@
+import TopNav from './TopNav';
+
+export default {
+  title: 'Layouts/TopNav',
+  component: TopNav,
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = () => <TopNav />;
+
+export const Primary = Template.bind({});
--- packages/client/src/components/Layouts/TopNav/TopNav.tsx
+++ packages/client/src/components/Layouts/TopNav/TopNav.tsx
@@ -1,4 +1,4 @@
-import CartIcon from '../Icons/CartIcon';
+import CartIcon from '../../Icons/CartIcon';
 
 const TopNav = () => {
   return (
